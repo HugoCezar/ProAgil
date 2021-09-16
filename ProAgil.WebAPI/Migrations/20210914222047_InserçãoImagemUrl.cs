602d5a2695ae1a1106a7fb1611a3eb980ed3c416
Hugo <hugoleocezar@hotmail.com>
2021-09-16 19:39:47 -0300

    Ad Component nav, add coluna Imagem.

---
+++ ProAgil.WebAPI/Migrations/20210914222047_InserçãoImagemUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class InserçãoImagemUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemUrl",
+                table: "Eventos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagemUrl",
+                table: "Eventos");
+        }
+    }
+}
